0c958d228fffd1b5d84bf38353e93db7d8a5f653
dodikk <dodikk88.reg@gmail.com>
2014-05-13 23:46:09 +0300

    Improved exception safety and C++11 support

--- ObjcScopedGuard/ObjcScopedGuard/ObjcScopedGuard.mm
+++ ObjcScopedGuard/ObjcScopedGuard/ObjcScopedGuard.mm
@@ -1,6 +1,40 @@
 #import "ObjcScopedGuard.h"
 
-using namespace Utils;
+using namespace ::Utils;
+
+void ObjcScopedGuard::SafeInvokeGuardBlock(GuardCallbackBlock callback_) throw()
+{
+    if ( nil == callback_ )
+    {
+        return;
+    }
+
+    
+    try
+    {
+        @try
+        {
+            callback_();
+        }
+        @catch ( NSException* exception )
+        {
+            NSLog( @"[ObjcScopedGuard] Obj-C exception in guard block : %@",  exception );
+        }
+        @finally
+        {
+            // IDLE
+        }
+    }
+    catch (const std::exception& ex)
+    {
+        NSLog( @"[ObjcScopedGuard] C++ exception in guard block : %s",  ex.what() );
+    }
+    catch (...)
+    {
+        NSLog( @"[ObjcScopedGuard] Unknown C++ exception in guard block" );
+    }
+
+}
 
 ObjcScopedGuard::ObjcScopedGuard( GuardCallbackBlock callback_ ) : 
     _block( [ callback_ copy ] ),
@@ -10,13 +44,17 @@ ObjcScopedGuard::ObjcScopedGuard( GuardCallbackBlock callback_ ) :
 
 ObjcScopedGuard::~ObjcScopedGuard() throw()
 {
-    if ( this->_isActive )
+    if ( !this->_isActive )
     {
-        _block();
+        return;
     }
+    
+    ObjcScopedGuard::SafeInvokeGuardBlock( this->_block );
 }
 
 void ObjcScopedGuard::Release() throw()
 {
     this->_isActive = false;
+    this->_block    = nil;
 }
+
