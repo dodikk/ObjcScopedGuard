813fd68b0a996239dc5b4f0abef0a904c068201d
dodikk <dodikk88.reg@gmail.com>
2014-12-06 19:06:15 +0200

    Added dynamic framework target and scheme for Carthage support

---
+++ ObjcScopedGuard/ObjcScopedGuard-DynamicFramework/ObjcScopedGuard-DynamicFramework.h
@@ -0,0 +1,19 @@
+//
+//  ObjcScopedGuard-DynamicFramework.h
+//  ObjcScopedGuard-DynamicFramework
+//
+//  Created by Oleksandr Dodatko on 06/12/2014.
+//  Copyright (c) 2014 Embedded Sources. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ObjcScopedGuard-DynamicFramework.
+FOUNDATION_EXPORT double ObjcScopedGuard_DynamicFrameworkVersionNumber;
+
+//! Project version string for ObjcScopedGuard-DynamicFramework.
+FOUNDATION_EXPORT const unsigned char ObjcScopedGuard_DynamicFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ObjcScopedGuard_DynamicFramework/PublicHeader.h>
+
+
